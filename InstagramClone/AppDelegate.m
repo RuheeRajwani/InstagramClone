6eff80b9037c5fb94a39d0b206bc6520288a8348
Ruhee Rajwani <ruheer@fb.com>
2022-06-27 15:29:35 -0700

    Connected to back4app

--- InstagramClone/AppDelegate.m
+++ InstagramClone/AppDelegate.m
@@ -6,11 +6,31 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
+
 
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(  NSDictionary *)launchOptions {
+
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource: @"Config" ofType: @"plist"];
+        NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
+       
+        configuration.applicationId = [dict objectForKey: @"APP_ID"]; // <- UPDATE
+        configuration.clientKey = [dict objectForKey: @"CLIENT_KEY"]; // <- UPDATE
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+
+    return YES;
+}
 
 @end
