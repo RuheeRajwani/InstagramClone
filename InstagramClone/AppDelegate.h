47b675ad6cb01ea5f7fb0d5c2d47502b1a8ab532
Ruhee Rajwani <ruheer@fb.com>
2022-06-27 14:50:15 -0700

    Initial Commit

---
+++ InstagramClone/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  InstagramClone
+//
+//  Created by Ruhee Rajwani on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
