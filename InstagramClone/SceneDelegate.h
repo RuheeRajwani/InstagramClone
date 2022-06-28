d7523ec2431eba079ef7da41597d63dbd4f881fe
Ruhee Rajwani <ruheer@fb.com>
2022-06-28 14:49:48 -0700

    Implemented persisting user session

--- InstagramClone/SceneDelegate.h
+++ InstagramClone/SceneDelegate.h
@@ -11,5 +11,7 @@
 
 @property (strong, nonatomic) UIWindow * window;
 
+- (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions;
+
 @end
 
--- InstagramClone/SceneDelegate.m
+++ InstagramClone/SceneDelegate.m
@@ -6,11 +6,26 @@
 //
 
 #import "SceneDelegate.h"
+#import <Parse/Parse.h>
+
 
 @interface SceneDelegate ()
 
 @end
 
 @implementation SceneDelegate
+
+- (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
+    
+    // Code to initialize Parse
+    // (See above section 'Parse `initializeWithConfiguration` vs `setApplicationId`', if you have not already set it up)
+    
+    if (PFUser.currentUser) {
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        
+        self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+    }
+
+}
 
 @end
