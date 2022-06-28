b32f89d9ac0e4138247a8ab9463a570e726002b8
Ruhee Rajwani <ruheer@fb.com>
2022-06-28 14:05:10 -0700

    Implemented login/logout

---
+++ InstagramClone/HomeFeedViewController.m
@@ -0,0 +1,61 @@
+//
+//  HomeFeedViewController.m
+//  InstagramClone
+//
+//  Created by Ruhee Rajwani on 6/28/22.
+//
+
+#import "HomeFeedViewController.h"
+#import "LoginViewController.h"
+#import "SceneDelegate.h"
+#import <Parse/Parse.h>
+
+
+@interface HomeFeedViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *arrayOfPosts;
+
+@end
+
+@implementation HomeFeedViewController
+
+- (IBAction)didTapLogout:(id)sender {
+    SceneDelegate *mySceneDelegate = (SceneDelegate * ) UIApplication.sharedApplication.connectedScenes.allObjects.firstObject.delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    mySceneDelegate.window.rootViewController = loginViewController;
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+    }];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return nil;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.arrayOfPosts.count;
+}
+
+
+
+
+@end
